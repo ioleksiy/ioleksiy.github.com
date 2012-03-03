8e68c764cd9b2a297726cf49836cf65781be848d
Oleksii Glib <oleksii@acropolium.com>
2012-03-03 03:45:43 +0200

    UI tweaks

--- assets/themes/twitter/js/blog.js
+++ assets/themes/twitter/js/blog.js
@@ -1,3 +1,49 @@
+jQuery.fn.sortElements = (function(){
+ 
+    var sort = [].sort;
+ 
+    return function(comparator, getSortable) {
+ 
+        getSortable = getSortable || function(){return this;};
+ 
+        var placements = this.map(function(){
+ 
+            var sortElement = getSortable.call(this),
+                parentNode = sortElement.parentNode,
+ 
+                // Since the element itself will change position, we have
+                // to have some way of storing its original position in
+                // the DOM. The easiest way is to have a 'flag' node:
+                nextSibling = parentNode.insertBefore(
+                    document.createTextNode(''),
+                    sortElement.nextSibling
+                );
+ 
+            return function() {
+ 
+                if (parentNode === this) {
+                    throw new Error(
+                        "You can't sort elements if any one is a descendant of another."
+                    );
+                }
+ 
+                // Insert before flag:
+                parentNode.insertBefore(this, nextSibling);
+                // Remove flag:
+                parentNode.removeChild(nextSibling);
+ 
+            };
+ 
+        });
+ 
+        return sort.call(this, comparator).each(function(i){
+            placements[i].call(getSortable.call(this));
+        });
+ 
+    };
+ 
+})();
+
 var list;
 var at = new Array();
 
@@ -44,6 +90,14 @@ function loadCss(url) {
 }
 
 $(function() {
+  $('ul.tag_box').each(function (idx, e){
+    $(e).children().sortElements(function(a, b){
+    	return $(a).text() > $(b).text() ? 1 : -1;
+	});
+  });
+  $('div.tag_box_data').sortElements(function(a, b){
+   	return $(a).children().first().children().first().text() > $(b).children().first().children().first().text() ? 1 : -1;
+  });
   list = $('div.gc');
   list.html('<pre>Loading sourcecode gist...</pre>');
   list.each(function (idx, e) {
